62a25bcf7ab28d6a5b3407ec486c5cf48eff37ad
Sous-tartar <97950336+Sous-tartar@users.noreply.github.com>
2023-04-25 10:47:29 +0300

    Добавил проверку на наличие лиц
    Если лица нет, то бот отправит сообщение вместо необработанного фото

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import telebot as tb
-import os
+# import os
 
 token = "6198886108:AAHdFZhy4M-XbVayyZHa1Ez_FrF3JSjACHw"
 bot = tb.TeleBot(token)
@@ -27,6 +27,11 @@ def start(message):
     for (x, y, w, h) in results:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
     cv2.imwrite('image2.jpg', img)
-    bot.send_photo(message.chat.id, (open("image2.jpg", "rb")), caption = "результат")
+
+
+    if len(results) != 0:
+        bot.send_photo(message.chat.id, (open("image2.jpg", "rb")), caption="результат")
+    else:
+        bot.send_message(message.chat.id, 'лиц на картинке не найдено')
 
 bot.polling(none_stop = True)
